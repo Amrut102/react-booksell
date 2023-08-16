f03ebf490a8e39dd35cbb2b1268d1a62da43b90a
Amrut Moghariya <amrutptl007@gmail.com>
2023-08-17 01:07:19 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,76 +1,61 @@
-import './App.css';
-import './index.js';
-import { Route, Routes, NavLink } from 'react-router-dom';
-import Welcome from './components/Welcome';
-import User from './components/User';
-import {CssBaseline} from '@material-ui/core';
-import Home from './components/Home';
-import { green, purple } from '@material-ui/core/colors';
-import { createTheme ,ThemeProvider } from '@material-ui/core/styles';
+import logo from './logo.svg';
+import './css/App.css';
+import EditUser from './Pages/EditUser';
+import globalStyles from './Components/Constants';
+import appStyle from './css/AppStyle.module.css';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './Components/Header';
+import { AuthProvider, useAuthContext } from './contexts/auth';
+import LoginProvider from './contexts/Loginprovider';
+import { AuthContext } from './contexts/auth';
+import { useContext } from 'react';
+import Footer from './Components/Footer';
+import UpdateProfile from './Pages/UpdateProfile';
+import { loginContext } from './contexts/LoginContext';
+import loader from '../src/assets/Loader-Icon.gif';
+import '../src/css/loader.css';
+import { MainNavigation } from './MainNavigation';
+import { CartProvider } from './contexts/cartContext';
+// import { MainNavigation } from './Components/MainNavigation';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      // Purple and green play nicely together.
-      // main: purple[500],
-      light: "#ff4081",
-      main: green[500], 
-      dark: "#0d1f0e",
-      contrastText: "#fff"
-    },
-    secondary: {
-      // This is green.A700 as hex.
-      main: '#061126',
-      // main: purple[200],
-
-    },
-  },
-  typography: {
-    fontFamily:'sans-serif',
-    fontSize:15,
-    h1: {
-      fontWeight: 300,
-      fontSize: "6rem",
-      lineHeight: 1.167,
-      letterSpacing: "-0.01562em",
-      color:"#061126 !important",
-    },
-    
-  },
-});
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+  // const authContext = useAuthContext();
   
-      <div >
-        <div style={
-          {
-            display: 'flex',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            background: 'linear-gradient(120deg, #305170,#6DFC6B)',
-            
-            height: 70
-          }
-        }>
-
-            <NavLink to='/' style={{fontSize:'1.2rem',textDecoration:'none',color:'Background'}}>HOME</NavLink><br />
-            <NavLink to='/welcome' style={{fontSize:'1.3rem',textDecoration:'none',color:'Background'}} > WELCOME </NavLink><br />
-            <NavLink to='/user' style={{fontSize:'1.3rem',textDecoration:'none',color:'Background'}}> USER </NavLink>
+  return (
+    <>
+      
+      <ToastContainer />
+      <BrowserRouter>
+      <LoginProvider>
+        <AuthProvider>
+        <CartProvider>
+        <div>
+        <div className="loader-wrapper">
+                <img src={loader} alt="loader" />
         </div>
+        </div>
+        <Header />
+        {/* {JSON.stringify(useAuthContext().user)} */}
+        <MainNavigation/>
+        <Footer/>
+        </CartProvider>
+        </AuthProvider>
+        </LoginProvider>
+      </BrowserRouter>
+     
+      
+      
+      
+      
+    </>);
 
-        <Routes path='/'>
-          <Route index element={<Home/>} />
-          <Route path='/welcome' element={<Welcome />} />
-          <Route path='/user' element={<User name="Amrut" role="student" />} />
-        </Routes>
-      </div>
-    </ThemeProvider>
 
-  );
+
+
+
 }
 
 export default App;
-
